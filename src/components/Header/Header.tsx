55d2444e1cf2737703c064bd2756911216bcd165
hellpains <rustam2004sadulaev@mail.ru>
2022-07-20 15:06:33 +0300

    folder structure

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import style from './Header.module.css'
 
 export const Header = () => {
     const img='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/VK_Compact_Logo.svg/768px-VK_Compact_Logo.svg.png'
 
     return (
-        <header className={'header'}>
+        <header className={style.header}>
             <img src={img} alt=""/>
             <span>ВКОНТАКТЕ</span>
         </header>
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import style from './Profile.module.css'
 
 export const Profile = () => {
     return (
-        <div className={'content'}>
+        <div className={style.content}>
             <div>
                 <img
                     src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300"
@@ -15,11 +16,11 @@ export const Profile = () => {
                     <div>
                         New posts
                     </div>
-                    <div>
-                        <div>
+                    <div className={style.posts}>
+                        <div className={style.item}>
                             post 1
                         </div>
-                        <div>
+                        <div className={style.item}>
                             post 2
                         </div>
                     </div>
